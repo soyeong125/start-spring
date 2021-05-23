74bde3cda60c5881352f210c68d7e2f9fdf72a24
SoYeong Choi <thdud3410@gmail.com>
2021-05-23 18:49:53 +0900

    머스테치 추가 스프링부트에서으 화면처리 방식

--- src/main/java/com/startspring/springboot/service/posts/PostsService.java
+++ src/main/java/com/startspring/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.startspring.springboot.service.posts;
 
 import com.startspring.springboot.domain.posts.Posts;
 import com.startspring.springboot.domain.posts.PostsRepository;
+import com.startspring.springboot.web.dto.PostsListResponseDto;
 import com.startspring.springboot.web.dto.PostsResponseDto;
 import com.startspring.springboot.web.dto.PostsSaveRequetDto;
 import com.startspring.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,17 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 업습니다. id =" + id));
+        postsRepository.delete(posts);
     }
 }
