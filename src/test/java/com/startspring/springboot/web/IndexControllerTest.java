728d84d0afb386278c0ed3e6c731b17b795fbf87
SoYeong Choi <thdud3410@gmail.com>
2021-06-20 19:59:19 +0900

    파일 추가

--- src/test/java/com/startspring/springboot/web/IndexControllerTest.java
+++ src/test/java/com/startspring/springboot/web/IndexControllerTest.java
@@ -10,17 +10,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("unchecked")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Test
     public void 메인페이지_로딩(){
         String body = this.restTemplate.getForObject("/",String.class);
-
         assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
